290042a80387fc1e9a490a48208cc928c1bd60ba
Lappy <roselleroxtabuena@gmail.com>
2021-06-29 10:34:58 +0800

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,25 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
-    end
+	def index
+		@portfolio_items = Portfolio.all
+	end
+
+	def new 
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+			respond_to do |format|
+				if @portfolio_item.save
+					format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
+				else
+					format.html { render :new, status: :unprocessable_entity }
+				end
+		end
+	end
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end
+
 end
