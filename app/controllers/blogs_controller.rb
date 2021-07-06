13395408496d9fb36852a23b5b3067b0ed481e0b
Roselle Tabuena <roselleroxtabuena@gmail.com>
2021-07-06 14:17:48 +0800

    Refractored strong params for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -75,6 +75,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog)
     end
 end
